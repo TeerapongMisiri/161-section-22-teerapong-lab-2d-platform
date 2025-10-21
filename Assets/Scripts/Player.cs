fc0a6e28166f0be390fdc409b8e93c211e41b12b
TeerapongMisiri <patterzalol@gmail.com>
2025-10-21 16:22:10 +0700

    Script 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : Character
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        base.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
